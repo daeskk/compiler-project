8127d1b73143c04b14be80a9df62ce14fb9981ff
arthur <arthur.daes@gmail.com>
2024-08-12 20:17:34 -0300

    converting to multimodule

---
+++ compiler-core/src/compiler/exceptions/SemanticException.java
@@ -0,0 +1,14 @@
+package compiler.exceptions;
+
+import java.io.Serial;
+
+public class SemanticException extends RuntimeException
+{
+	@Serial
+	private static final long serialVersionUID = 1L;
+	
+	public SemanticException(String message)
+	{
+		super(message);
+	}
+}
