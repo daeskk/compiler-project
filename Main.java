2fb4bfc3ed619c41a0fe208d2ae071a409a04214
arthur <arthur.daes@gmail.com>
2024-09-08 18:48:01 -0300

    update readme and input test

--- Main.java
+++ Main.java
@@ -1,63 +1,53 @@
-import java.util.*;
+import java.util.*; 
+public class Main { 
+	public static void main(String args[]) {
+		Scanner _sc = new Scanner(System.in);
+		int a;
+		int b;
+		double c;
+		int d;
+		String waowMyString;
+		String testString2;
+		waowMyString = "helloioii";
+		a = 2;
+		b = 1+2*3/a;
+		System.out.println("a variavel b vale ");
+		System.out.println(b);
+		c = _sc.nextDouble(); _sc.nextLine();
+		testString2 = "helloioiiasdasd";
+		System.out.println(a);
+		if (b!=5) {
+System.out.println(b);
+if (b==4) {
+System.out.println("b igual a 4");
+if (b+1==5) {
+System.out.println("b mais um fica igual a 5");
+}
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner _sc = new Scanner(System.in);
-        int a;
-        int b;
-        double c;
-        int d;
-        String waowMyString;
-        String testString2;
-        waowMyString = "helloioii";
-        a = 1;
-        b = 1 + 2 * 3 / a;
-        System.out.println("a variavel b vale ");
-        System.out.println(b);
-        c = _sc.nextDouble();
-        _sc.nextLine();
-        testString2 = "helloioiiasdasd";
-        System.out.println(a);
-        if (b != 5)
-        {
-            System.out.println(b);
-            if (b == 3)
-            {
-                System.out.println("b igual a 3");
-                if (b + 1 == 4)
-                {
-                    System.out.println("b igual a 4");
-                }
+}
 
-            }
+}
 
-        }
+		do {
+System.out.println(b);
+b = b+5*3;
+}while (b<50);
 
-        do
-        {
-            System.out.println(b);
-            b = b + 5 * 3;
-        } while (b < 50);
+		System.out.println("oi aqui esta o valor da soma");
+		System.out.println(b);
+		c = 20.0;
+		while (c<=111.11) {
+System.out.println("legal");
+if (c>112.0) {
+System.out.println("passou parando");
+break;
 
-        System.out.println("oi aqui esta o valor da soma");
-        System.out.println(b);
-        c = 20.0;
-        while (c <= 111.11)
-        {
-            System.out.println("legal");
-            if (c > 112.0)
-            {
-                System.out.println("passou parando");
-                break;
+}
 
-            }
+c = c+15.9;
+}
 
-            c = c + 15.9;
-        }
+		_sc.close();
 
-        _sc.close();
-
-    }
+	}
 }
--- README.md
+++ README.md
@@ -4,3 +4,25 @@ command to generate ANTLR source files:
 ```
 java -cp antlr-4.13.2-complete.jar org.antlr.v4.Tool IsiGrammar.g4 -o compiler-core/src/compiler/core -package compiler.core
 ```
+
+## Youtube video link
+https://www.youtube.com/
+
+## Project members
+<table> 
+	<thead>
+	<th>Nome</th>
+	<th>RA</th>
+	</thead>
+	<tbody>
+	<tr>
+		<td>Arthur Dae Sung Han</td>
+		<td>11202232206</td>
+	</tr>
+	<tr>
+		<td>Integrante 2</td>
+		<td></td>
+	</tr>
+	</tbody>
+</table>
+ 
